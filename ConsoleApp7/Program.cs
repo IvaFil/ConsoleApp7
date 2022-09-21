8dd21d5248c6c616bdae6ba3e5d4e3a3e4d7a3da
IvaFil <filipovichiv@yandex.ru>
2022-09-21 11:09:01 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введи число: ");
+
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            number /= 10;
+            number %= 10;
+            Console.WriteLine(number);
+        }
+    }
+}
